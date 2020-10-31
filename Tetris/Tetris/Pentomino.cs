4da23d0ea0b8d0fbf05a1185a79868d994c8e8d5
tomek1kk <tomek1kkgw@gmail.com>
2020-10-31 22:49:46 +0100

    reading pieces

--- Tetris/Tetris/Pentomino.cs
+++ Tetris/Tetris/Pentomino.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,6 +29,26 @@ namespace Tetris
         private void radioButton2_CheckedChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (loadFromFileRadio.Checked)
+            {
+                var reader = new StreamReader("pieces.txt");
+                string line = reader.ReadLine();
+                var counts = line.Split(' ');
+
+            }
+            else
+            {
+                var random = new Random();
+                for (int i = 0; i < pieceCounter.Value; i++)
+                {
+                    var count = random.Next(0, 11);
+
+                }
+            }
         }
     }
 }
