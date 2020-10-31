1f59dcd060e56ff044a9cd453ffda18ab4505c9f
Piotr Kośkiewicz <piotr888k@gmail.com>
2020-10-31 21:07:38 +0100

    Add first version of board and pieces

---
+++ Tetris/Tetris/Board.cs
@@ -0,0 +1,49 @@
+﻿namespace Tetris
+{
+    public class Board
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Types[,] Fields { get; set; }
+
+        public Board(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            InitializeFields();
+        }
+
+        public bool CanPieceBePlaced(int x, int y, Point[] points)
+        {
+            foreach (Point point in points)
+            {
+                if(Fields[x + point.X, y + point.Y] != Types.Empty)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public void PlacePiece(int x, int y, Point[] points, Types type)
+        {
+            foreach (Point point in points)
+            {
+                Fields[x + point.X, y + point.Y] = type;
+
+            }
+        }
+
+        private void InitializeFields()
+        {
+            Fields = new Types[Height, Width];
+            for (int i = 0; i < Fields.GetLength(0); i++)
+            {
+                for (int j = 0; j < Fields.GetLength(1); j++)
+                {
+                    Fields[i, j] = Types.Empty;
+                }
+            }
+        }
+    }
+}
---
+++ Tetris/Tetris/Pentominos.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Tetris
+{
+    public static class Pentominos
+    {
+        public static Dictionary<Types, Point[]> pentominos = new Dictionary<Types, Point[]>()
+        {
+            {Types.F, new Point[] {new Point(1, 0), new Point(0, 1) , new Point(1, 1) , new Point(2, 1) , new Point(0, 2) } },
+            {Types.I, new Point[] {new Point(0, 0), new Point(1, 0) , new Point(2, 0) , new Point(3, 0) , new Point(4, 0) } },
+            {Types.L, new Point[] {new Point(0, 0), new Point(1, 0) , new Point(2, 0) , new Point(3, 0) , new Point(3, 1) } },
+            {Types.N, new Point[] {new Point(2, 0), new Point(3, 0) , new Point(0, 1) , new Point(1, 1) , new Point(2, 1) } },
+            {Types.P, new Point[] {new Point(0, 0), new Point(1, 0) , new Point(2, 0) , new Point(0, 1) , new Point(1, 1) } },
+            {Types.T, new Point[] {new Point(0, 0), new Point(0, 1) , new Point(0, 2) , new Point(1, 1) , new Point(2, 1) } },
+            {Types.U, new Point[] {new Point(0, 0), new Point(0, 2) , new Point(1, 0) , new Point(1, 1) , new Point(1, 2) } },
+            {Types.V, new Point[] {new Point(0, 0), new Point(1, 0) , new Point(2, 0) , new Point(0, 1) , new Point(0, 2) } },
+            {Types.W, new Point[] {new Point(0, 0), new Point(1, 0) , new Point(1, 1) , new Point(2, 1) , new Point(2, 2) } },
+            {Types.X, new Point[] {new Point(1, 0), new Point(0, 1) , new Point(1, 1) , new Point(2, 1) , new Point(1, 2) } },
+            {Types.Y, new Point[] {new Point(1, 0), new Point(0, 1) , new Point(1, 1) , new Point(2, 1) , new Point(3, 1) } },
+            {Types.Z, new Point[] {new Point(0, 0), new Point(0, 1) , new Point(1, 1) , new Point(2, 1) , new Point(2, 2) } }
+        };
+    }
+}
