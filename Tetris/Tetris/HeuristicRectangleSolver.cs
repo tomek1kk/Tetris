137dc4c4ff80f5a2b035582d4b2b5bb86970ea8b
Polina Grinko <grinkopolina@gmail.com>
2020-11-01 19:51:46 +0100

    fixed compilation errors

---
+++ Tetris/Tetris/HeuristicRectangleSolver.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tetris
+{
+    internal class HeuristicRectangleSolver
+    {
+        internal static void Solve(List<Pentomino> pentominos, Board board)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
