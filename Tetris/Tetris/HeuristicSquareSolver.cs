43a067088721947fd065e9d15ec5e504660cf3d1
Polina Grinko <grinkopolina@gmail.com>
2020-11-02 22:29:35 +0100

    improved heuristic square

--- Tetris/Tetris/HeuristicSquareSolver.cs
+++ Tetris/Tetris/HeuristicSquareSolver.cs
@@ -23,35 +23,37 @@ namespace Tetris
 			var positions = Solver.PotentiallyValidPositions(polyminos, board.Width, board.Height);
 			foreach (var t in polyminos)
 			{
-				foreach(var p in positions[t.Type])
+				var rating = new Dictionary<(Polymino, Point), int>();
+				foreach (var p in positions[t.Type])
 				{
-					var rating = new Dictionary<int, (Polymino, Point)>();
+					
 					foreach (var rotated_polymino in t.Rotations())
 					{
-						rating.Add(board.RatePosition(p.X, p.Y, rotated_polymino), (rotated_polymino, p));
-					}
-					(Polymino polymino, Point position)? best_position = FindBestRating(rating);
-					if (best_position == null)
-					{
-						return null; //no solution found, no place to put all pentominos in current board
-					}
-					else
-					{
-						board.PlacePolymino(best_position.Value.position.X, best_position.Value.position.Y, best_position.Value.polymino);
+						rating.Add((rotated_polymino, p), board.RatePosition(p.X, p.Y, rotated_polymino));
 					}
 				}
+				(Polymino polymino, Point position)? best_position = FindBestRating(rating);
+				if (best_position == null)
+				{
+					return null; //no solution found, no place to put all pentominos in current board
+				}
+				else
+				{
+					board.PlacePolymino(best_position.Value.position.X, best_position.Value.position.Y, best_position.Value.polymino);
+				}
 			}
-			return null;
+			return new List<Board>(){ board};
 		}
 
 
-		public static (Polymino polymino, Point position)? FindBestRating(Dictionary<int, (Polymino, Point)> rating)
+		public static (Polymino polymino, Point position)? FindBestRating(Dictionary<(Polymino, Point), int> rating)
 		{
-			if (rating.Keys == null)
+			if (rating.Values == null)
 			{
 				return null;
 			}
-			return rating[rating.Keys.Max()];
+
+			return rating.FirstOrDefault(x => x.Value == rating.Values.Max()).Key;
 		}
 	}
 }
